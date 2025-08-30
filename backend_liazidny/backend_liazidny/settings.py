71f90be590bd074c41ae50bd47e24c9bf5f24f77
Ony <onymcraft@gmail.com>
2025-08-30 12:35:43 +0700

    Final configuration for Render deployment

--- backend_liazidny/backend_liazidny/settings.py
+++ backend_liazidny/backend_liazidny/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9(^ztx@9b&7e3f0ia=g1*tla=8lkt@2&shc*6q=2+-2u#fy_#0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -85,17 +85,18 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 import os
+import dj_database_url
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
